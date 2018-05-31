cdbf8dc9e8cee05bba8ff325d0ba720402465796
MinhThanhHua <minhthanhhua17@gmail.com>
2018-05-31 11:15:25 +0700

    updated JS EXE 1

--- slide-image/assets/js/script.js
+++ slide-image/assets/js/script.js
@@ -1,11 +1,33 @@
+window.onload = function() {
+	setInterval(function(){ 
+		changeImageClick("phai");
+	}, 3000);
+}
+// thay đổi độ mờ của ảnh
+function changeOpacity() {
+	var image = document.getElementById("imageshow");
+	var opacity =0.5;
+	changOp();
+	function changOp() {
+		if(opacity<0.9) {
+			opacity += 0.1;
+			setTimeout(function() {
+				changOp();
+			},200);
+			image.style.opacity = opacity;
+		}
+	}
+}
 // đổi ảnh khi click vào ảnh nhỏ
 function changeImage(name) {
+	changeOpacity();
 	var image = document.getElementById("imageshow");
 	image.src = "image/"+name+".jpg";
 	image.name= name;
 }
 // đổi ảnh khi bấm sang trái hoặc phải
 function changeImageClick(viTri) {
+	changeOpacity();
 	var image = document.getElementById("imageshow");
 	var name = getName(viTri);
 	image.src = "image/"+name+".jpg";
@@ -31,7 +53,6 @@ function getName(viTri) {
 		for (i = 0; i < arr.length; i++){
 			if (arr[i] == image.name){
 				if(i === 0) {
-					console.log("trai")
 					return arr[3];
 				}
 				return arr[i-1];
@@ -42,7 +63,6 @@ function getName(viTri) {
 		for (i = 0; i < arr.length; i++){
 			if (arr[i] === image.name){
 				if((arr.length-1) == i) {
-					console.log("phai")
 					return arr[0];
 				}
 				return arr[i+1];
