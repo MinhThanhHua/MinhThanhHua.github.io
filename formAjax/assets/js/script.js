2337e7916528d71bfc4fcb46dbe51a3575a224f0
MinhThanhHua <minhthanhhua17@gmail.com>
2018-06-04 17:58:59 +0700

    Form ajax 70%

--- formAjax/assets/js/script.js
+++ formAjax/assets/js/script.js
@@ -9,11 +9,13 @@ function HTTP() {
 var http = HTTP();
 
 function checkValidate() {
+	console.log('ok')
 	http.open('get', 'sever.php?data=true', true);
 	http.onreadystatechange = process;
 	http.send(null);
 }
 function process() {
+	console.log('ok')
 	if (http.readyState == 4 || http.status == 200) {
 		kq = http.responseText;
 		console.log(kq)
@@ -22,21 +24,60 @@ function process() {
 }
 // check validate form
 function checkValidateForm() {
+	var divInput = document.getElementsByClassName('input-control');
 	var userName = document.getElementsByName('username');
 	var password = document.getElementsByName('password');
 	var email = document.getElementsByName('email');
-	if (userName[0].value == '') {
-		message(0, 'Chưa nhập tên')
-	}consolelog('ok')
-	return false;
+	var ngaySinh = document.getElementsByName('ngaysinh');
+	var divInput = document.getElementsByClassName('input-control');
+	var show = document.getElementsByClassName('show');
+	if (userName[0].value == '' || userName[0].value.length <8) {
+		show[0].innerHTML = 'Username length min 8 letter';
+		return false;
+	}
+	show[0].innerHTML = '';
+	if (password[0].value == '' || password[0].value.length <8) {
+		show[1].innerHTML = 'Password length min 8 letter';
+		return false;
+	}
+	show[1].innerHTML = '';
+	if (checkEmail(email[0].value)) {
+		show[2].innerHTML = 'Email wrong format';
+		return false;
+	}
+	show[2].innerHTML = '';
+	if (checkBirthday(ngaySinh[0].value)){
+		show[3].innerHTML = 'Wrong Birthday';
+		return false;
+	}
+	show[3].innerHTML = '';
+	console.log('vao')
+	return true;
 }  
-// xuất thông báo
-function message(viTri, noiDung) {
-	var divInput = document.getElementsByClassName('input-control')
-	console.log(divInput[0])
-
-	var text = document.createTextNode(noiDung);
-	span.appendChild(text);
-	var ten = divInput[viTri].appendChild(span);
-	console.log(ten)
+// validate ngày sinh
+function checkBirthday(ngaySinh) {
+	var arr = ngaySinh.split('/');
+	var date = new Date();
+	var dateInput = new Date(arr[2], arr[1]-1, arr[0]);
+	if (date.getTime() < dateInput.getTime() || ngaySinh == ''){
+		return 1;
+	}
+	return 0;
 }
+
+// validate email
+function checkEmail(email) {
+	var arrEmail = email.split('@');
+	var emailTail = arrEmail[1];
+	if (emailTail == undefined || arrEmail[0].length < 3) {
+		return 1;
+	}
+	var arrEmailTail = emailTail.split('.');
+	if (arrEmailTail.length < 2 || arrEmailTail[0].length < 1 || arrEmailTail[1].length < 1 
+		|| !isNaN(arrEmailTail[0]) || !isNaN(arrEmailTail[1])) {
+		return 1;
+	}
+	return 0;
+}
+// /\@|\./
+// chỉ cần 1 giá trị undefine là submit => vãi :)))
