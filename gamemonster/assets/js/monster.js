22d8a1fc6bcd6f46f712787ed09b81e1e886e552
MinhThanhHua <minhthanhhua17@gmail.com>
2018-06-11 09:11:11 +0700

    xong game monster

--- gamemonster/assets/js/monster.js
+++ gamemonster/assets/js/monster.js
@@ -6,7 +6,6 @@ var ctxMain = canvasMain.getContext('2d');
 //random
 // var cx = Math.floor(Math.random()*(canvasMain.width-100));
 // var cy = Math.floor(Math.random()*(canvasMain.height-100))
-function listMonster(){
     var monster = new Monster(canvasMain.width, canvasMain.height, 1, 1, 170, 170);
     var monster1 = new Monster(canvasMain.width, canvasMain.height, 1, 1, 0, 0);
     var monster2 = new Monster(canvasMain.width, canvasMain.height, 0, 1, 170, 0);
@@ -18,14 +17,12 @@ function listMonster(){
     var monster8 = new Monster(canvasMain.width, canvasMain.height, -1, 1, 350, 400);
     var monsters = [monster, monster1, monster2, monster3, monster4, monster5, monster6,
         monster7, monster8];
-    return monsters;
-}
-var monsters = listMonster();
-
+    var oldMonster = [monster, monster1, monster2, monster3, monster4, monster5, monster6,
+        monster7, monster8];
 //-------------------Image -------------------------
 // Hình ảnh menu
 var imgMenuBg = new Image();
-
+imgMenuBg.src = 'image/menubg.png';
 
 var imgBoomBb = new Image();
 imgBoomBb.src = 'image/boombbb.png';
@@ -36,6 +33,8 @@ imgPause.src = 'image/pause.png';
 var imgReload = new Image();
 imgReload.src = 'image/reload.png';
 
+var imgHeart = new Image();
+imgHeart.src = 'image/heart.png'
 
 // Hình ảnh main
 var imgMainBg = new Image();
@@ -46,6 +45,12 @@ imgMonster.src = 'image/monster.png'
 
 var imgBoom = new Image();
 imgBoom.src = 'image/boom.png';
+
+var imgLose = new Image();
+imgLose.src = 'image/over.png';
+
+var imgBlood = new Image();
+imgBlood.src = 'image/blood.png';
 //------------------End Image---------------------------------
 
 //------------------- Layout --------------------------------
@@ -81,6 +86,12 @@ function Monster(mapWidtch, mapHeight, speedX, speedY, cx, cy) {
     this.speedY = speedY;
     this.cx = cx;
     this.cy = cy;
+    this.oldCx = cx;
+    this.oldCy = cy;
+}
+function blood(bx, by){
+    this.bx = bx;
+    this.by = by;
 }
 // tạo monster xuất hiện cố định
 // tạo ảnh monster
@@ -100,10 +111,10 @@ Monster.prototype.move = function () {
 }
 // xử lý va chạm
 Monster.prototype.checkCollision = function () {
-    if (this.left <= 0 || this.right >= canvasMain.width) {
+    if (this.left < 0 || this.right > canvasMain.width) {
         this.speedX = -this.speedX;
     }
-    if (this.top <= 0 || this.bottom > canvasMain.height) {
+    if (this.top < 0 || this.bottom > canvasMain.height) {
         this.speedY = -this.speedY;
     }
 }
@@ -112,10 +123,19 @@ Monster.prototype.monsterStatus = function (e) {
     preX = e.pageX - canvasMain.offsetLeft;
     preY = e.pageY - canvasMain.offsetTop;
     if (this.cx + 25 < preX && preX < this.cx + 100 
-        && this.cy + 10 < preY && preY < this.cy + 90 &&  this.transparen == true) {
+        && this.cy + 10 < preY && preY < this.cy + 90 &&  this.transparen == true 
+        && startStatus == true) {
         this.transparen = false;
-        randomMonster();
+        blood.bx = this.cx;
+        blood.by = this.cy;  
+        slMonster--;
+        if (slMonster == 0){
+            score(10);
+            randomMonster();
+            return;
+        }
         score(10);
+        
     }
     // if (0 < preX && preX < this.cx ){
     //     console.log('ok')
@@ -148,11 +168,15 @@ function update() {
             }
         }
         drawMonster(soLuongMonster);
+        checkHeart();
     }
 }
 // mỗi bước đi tiến hành vẽ lại
 function drawMonster(soLuongMonster) {
     layoutMain();
+    if (countClickMonster > 0){
+        ctxMain.drawImage(imgBlood, blood.bx+25, blood.by+25, 100 , 100)
+    }
     for (countMonster = 0; countMonster < soLuongMonster; countMonster++) {
         if (monsters[countMonster].transparen) {
             monsters[countMonster].draw();
@@ -172,13 +196,15 @@ function mouseClickMenu(name, toaDoXMin, toaDoXMax, toaDoYMin, toaDoYMax) {
     canvasMenu.addEventListener('click', function (e) {
         preX = e.pageX - canvasMenu.offsetLeft;
         preY = e.pageY - canvasMenu.offsetTop;
-        if (toaDoXMin < preX && preX < toaDoXMax && toaDoYMin < preY && preY < toaDoYMax) {
-            if (name == 'boom') {
-                killAll();
-            }
-            if (name == 'pause') {
-                startStatus = !startStatus;
-                update();
+        if (toaDoXMin < preX && preX < toaDoXMax && toaDoYMin < preY && preY < toaDoYMax ) {
+            if (countHeart >= 0){
+                if (name == 'boom' && startStatus == true) {
+                    killAll();
+                }
+                if (name == 'pause') {
+                    startStatus = !startStatus;
+                    update();
+                }
             }
             if (name == 'reload') {
                 location.reload();
@@ -186,10 +212,21 @@ function mouseClickMenu(name, toaDoXMin, toaDoXMax, toaDoYMin, toaDoYMax) {
         }
     })
 }
+// lcik vào monster
+var countClickMonster = 0;
 function mouseClickMonster(i) {
     canvasMain.addEventListener('click', function (e) {
         monsters[i].monsterStatus(e);
     })
+}
+// click vào canvas
+var countClickCanvasMain = 0;
+function canvasClick(){
+    canvasMain.addEventListener('click', function (e) {
+        if (startStatus){
+            countClickCanvasMain++;
+        }  
+    }) 
 }
 //kill all
 function killAll() {
@@ -228,11 +265,34 @@ function mouse() {
 // score
 var diem = 0;
 function score(number) {
+    if(number > 0){
+        countClickMonster++;
+    }
+    if (diem > 50 && slMonster == 0){
+        slMonster = 2;
+        console.log(slMonster)
+    } 
+    if (diem <= 50 && slMonster == 0){
+        slMonster = 1;
+    }
     diem += number;
+    //localStorage.myScore = 0;
     ctxMenu.beginPath();
     ctxMenu.clearRect(100, 10, 40, 20);
     ctxMenu.fillText(diem, 100, 30);
     ctxMenu.closePath();
+    highCore(diem);
+}
+// điểm cao
+function highCore(diem) {
+    let hiCore = localStorage.myScore;
+    console.log(localStorage.myScore)
+    if (diem >= hiCore){
+        localStorage.myScore = diem;
+        hiCore = diem;
+    }
+    ctxMenu.clearRect(180, 0, 50, 30)
+    ctxMenu.fillText(hiCore, 180, 30);
 }
 // pause or play
 function fauseOrPlay(startStatus) {
@@ -244,25 +304,79 @@ function fauseOrPlay(startStatus) {
 //-----------------Số lượng monster xuất hiện--------------------
 //xuất ra so luong 
 var soLuongMonster = 9
+var slMonster = 1;
 function randomMonster() {
+    let sl = 0;
+    let tt = -1;
+    for (sl = 0; sl < slMonster; sl++){
+        
+        do{
+            var randomMonst = random();
+        } while(randomMonst == tt)
+        tt = randomMonst;
+        monsters[randomMonst].transparen = true;
+        monsters[randomMonst].cx = monsters[randomMonst].oldCx;
+        monsters[randomMonst].cy = monsters[randomMonst].oldCy;
+        mouseClickMonster(randomMonst);
+    }
+}
+function random(){
     let randomMonst = Math.floor(Math.random() * (soLuongMonster));
-    let monsterNow = randomMonst;
-    monsters[randomMonst].transparen = true;
-    mouseClickMonster(randomMonst);
+    return randomMonst;
+}
+function slMonsters() {
+    level ++;
+    slMonster = level;
+}
+// heart
+var countHeart = 3;
+function checkHeart() {
+    if(countClickCanvasMain > countClickMonster ){
+        score(-10);
+        if (countHeart == 3){
+            clearHeart(140, 40);
+        }
+        if(countHeart == 2){
+            clearHeart(110, 40);
+        }
+        if (countHeart == 1){
+            clearHeart(80, 40);
+        }
+        if (countHeart == 0){
+            startStatus = false;
+            ctxMain.drawImage(imgLose, 50, 100, 350, 350);
+            console.log('ok')
+        }
+        countHeart--;
+        countClickCanvasMain = countClickMonster; 
+    }
 }
 
+//draw heart
+function drawHeart() {
+    ctxMenu.drawImage(imgHeart, 80, 40, 25, 25);
+    ctxMenu.drawImage(imgHeart, 110, 40, 25, 25);
+    ctxMenu.drawImage(imgHeart, 140, 40, 25, 25);
+}
+// clear heart
+function clearHeart(hx, hy) {
+    ctxMenu.clearRect( hx, hy, 25, 25)
+}
 
 //------------------Kết thúc------------------------------------
+var level = 1;
 window.onload = function main() {
     let speed = 100;
     let countMonster = 0;
     layoutMenu();
-    layoutMain();
+    layoutMain();  
     mouseClickMenu('boom', 240, 275, 50, 85);//Boom Kill All
     mouseClickMenu('pause', 320, 360, 50, 85);//pause
     mouseClickMenu('reload', 390, 420, 50, 85);//reload
     randomMonster();
+    canvasClick();
     fauseOrPlay(startStatus);
+    drawHeart();
     
 }
 
